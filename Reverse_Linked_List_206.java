c7c922ae5903ac7ce1d0ec3ffaa10dadf6530500
kevinlee168 <leezk007@leezks-MacBook-Pro.local>
2022-05-06 22:50:48 +0800

    Reverse_Linked_List_206

---
+++ Reverse_Linked_List_206.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class Reverse_Linked_List_206 {
+    /**
+     * 思路：思路：一遍遍历链表，一遍使用头插法将节点插入到新链表中
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = new ListNode();
+        newHead.next = null;
+        ListNode p;
+
+        //头插法
+        while (head != null) {
+            p = head;
+            // 先把原链表的头节点往后移动
+            head = head.next;
+            //头插法
+            p.next = newHead.next;
+            newHead.next = p;
+        }
+
+        return newHead.next;
+    }
+}
